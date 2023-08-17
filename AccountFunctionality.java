58a1cd2155d79d1ed6d732b4ad889bb55d5b93a9
somaye24 <132242568+somaye24@users.noreply.github.com>
2023-08-17 07:21:15 -0700

    upload Java codes

---
+++ AccountFunctionality.java
@@ -0,0 +1,39 @@
+package bankFunctionality;
+
+public class AccountFunctionality {
+	
+	String accountNumber;
+	double balance;
+	
+	public AccountFunctionality(String accountNumber, double balance) {
+		this.accountNumber = accountNumber;
+		this.balance = balance;
+	}
+	public void deposite(double amount) {
+		balance = amount+balance;
+	}
+	public void withdraw(double amount) {
+		if(balance>=amount) {
+			balance = balance - amount;
+		}
+			else {
+				System.out.println("The balance is not sufficient");
+			}
+	}
+	public double getBalance() {
+		return balance;
+	}
+	
+	public static void main(String[] args) {
+	
+		AccountFunctionality BA1234 = new AccountFunctionality("BA1234",800);
+		BA1234.deposite(300);
+		System.out.println("New balance is : "+BA1234.getBalance());
+		
+		BA1234.withdraw(600);
+		System.out.println("New balance is : "+BA1234.getBalance());
+		
+		BA1234.withdraw(900);
+		System.out.println("New balance is : "+BA1234.getBalance());
+}
+}
